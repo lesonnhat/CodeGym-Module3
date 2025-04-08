64170938dd7941b8f4565204b274dae70912693d
Your Namelesonnhat <sonnhatbest@gmail.com>
2025-04-08 13:47:33 +0700

    add lesson 12

--- Week3/Lesson12/user.sql
+++ Week3/Lesson12/user.sql
@@ -29,3 +29,27 @@ BEGIN
  END$$
 DELIMITER ;
 
+create table Permision(
+      id int(11) primary key,
+      name varchar(50)
+);
+
+create table User_Permision(
+     permision_id int(11),
+     user_id int(11)
+);
+
+insert into Permision(id, name) values(1, 'add');
+insert into Permision(id, name) values(2, 'edit');
+insert into Permision(id, name) values(3, 'delete');
+insert into Permision(id, name) values(4, 'view');
+
+select * from Permision;
+
+create table Employee ( 
+            id int(3) NOT NULL AUTO_INCREMENT, 
+            name varchar(120) NOT NULL, 
+            salary int(220) NOT NULL, 
+            created_Date datetime, 
+            PRIMARY KEY (id) 
+);
